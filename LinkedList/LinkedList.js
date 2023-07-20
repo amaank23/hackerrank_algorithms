f4d5996daa836184202b48fac5dfd399aee78241
Aman <=>
2023-07-20 16:59:39 +0500

    updated

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -28,6 +28,7 @@ class LinkedList {
     let lastNode = this.head;
     if (lastNode) {
       while (lastNode.next) {
+        size
         lastNode = lastNode.next;
       }
     }
@@ -142,15 +143,77 @@ class LinkedList {
 }
 
 let list = new LinkedList();
-list.insertNodeAtTail(141);
-list.insertNodeAtTail(302);
-list.insertNodeAtTail(3302);
-list.insertNodeAtTail(455);
-// list.insertNodeAtTail(164);
-// list.insertNodeAtTail(530);
-// list.insertNodeAtTail(474);
-// list.insertNodeAtHead(1456);
-// list.insertNodeAtHead(987);
-// list.insertNodeAtPosition(7853, 3);
-list.reverse();
-list.printLinkedList();
+list.insertNodeAtTail(1);
+list.insertNodeAtTail(2);
+list.insertNodeAtTail(3);
+
+let list2 = new LinkedList();
+list2.insertNodeAtTail(3);
+list2.insertNodeAtTail(4);
+// list.reverse();
+// list.printLinkedList();
+
+function compareLinkedLists(list1, listTwo) {
+  let node1 = list1.head;
+  let node2 = listTwo.head
+  while (node1 || node2) {
+    if (node1 === null || node2 === null) {
+      return 0
+    }
+    if (node1.data === node2.data) {
+      node1 = node1.next;
+      node2 = node2.next;
+    } else {
+      return 0
+    }
+  }
+  return 1
+}
+
+function mergeLists(list1, list2) {
+  let node1 = list1.head;
+  let node2 = list2.head;
+  let newList = new LinkedList()
+  // newList.head = new ListNode(1) 
+  if (node1.data <= node2.data) {
+    newList.head = new ListNode(node1.data)
+    newList.head.next = new ListNode(node2.data)
+  } else {
+    newList.head = new ListNode(node2.data)
+    newList.head.next = new ListNode(node1.data)
+  }
+
+  let newLList = newList.head.next;
+  node1 = node1.next;
+  node2 = node2.next;
+
+  while (newLList) {
+    if (node1 && node2) {
+      if (node1.data <= node2.data) {
+        newLList.next = new ListNode(node1.data)
+        newLList.next.next = new ListNode(node2.data)
+      } else {
+        newLList.next = new ListNode(node2.data)
+        newLList.next.next = new ListNode(node1.data)
+      }
+      node1 = node1.next;
+      node2 = node2.next;
+      newLList = newLList.next
+    } else if (node1 && node2 === null) {
+      newLList.next.next = new ListNode(node1.data)
+      node1 = node1.next;
+      newLList = newLList.next
+    } else if (node2 && node1 === null) {
+      newLList.next = new ListNode(node2.data)
+      node2 = node2.next;
+      newLList = newLList.next
+    } else {
+      break
+    }
+  }
+
+  newList.printLinkedList()
+
+
+}
+mergeLists(list, list2)
